6d8c24ff298da395dff31b301552962a050bbf6f
namahxverma <78422591+namahxverma@users.noreply.github.com>
2021-03-16 23:52:59 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,18 @@
+class dustbin {
+  constructor(x, y, width, height) {
+    this.body = Bodies.rectangle(x, y, width, height,{isStatic:true});
+    this.width = width ; 
+    this.height = height;
+this.image=loadImage("dustbin image.jpeg");
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+
+    imageMode(CENTER);
+    fill("green");
+    image(this.image,pos.x,pos.y);
+
+  }
+}
